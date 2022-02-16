3b9b67edc7f2076eb94fd64835d06540c080276e
Artuqu <percikartur@gmail.com>
2022-02-16 20:38:29 +0100

    Login view

--- src/main/java/pl/coderslab/javafx/HelloApplication.java
+++ src/main/java/pl/coderslab/javafx/HelloApplication.java
@@ -4,15 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+//    public static final String title = "Enterprise App";
+    public static final int width = 600;
+    public static final int height = 400;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), width, height);
+//        stage.setTitle(title);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
