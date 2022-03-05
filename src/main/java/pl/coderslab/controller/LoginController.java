c9bd832791098161f06bf43f9ed23a56160ff641
Artuqu <percikartur@gmail.com>
2022-03-05 22:28:51 +0100

    Button support in login panel

--- src/main/java/pl/coderslab/controller/LoginController.java
+++ src/main/java/pl/coderslab/controller/LoginController.java
@@ -3,21 +3,58 @@ package pl.coderslab.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import pl.coderslab.factory.PopupFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
 
-    @FXML
-    private Button onExitButtonClick;
+    private Stage getStage() {
+        return (Stage) loginAnchorPane.getScene().getWindow();
+    }
+
+    private PopupFactory popupFactory;
+
+    public LoginController() {
+        popupFactory = new PopupFactory();
+    }
 
     @FXML
-    private VBox onLoginButtonClick;
+    private Button exitButton;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private AnchorPane loginAnchorPane;
+    @FXML
+    private TextField loginTextField;
+    @FXML
+    private TextField passwordTextField;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        onExitButtonClick.setOnAction(x -> System.out.println("Works"));
+        exitButtonInitialize();
+        loginButtonInitialize();
+    }
+
+    private void loginButtonInitialize() {
+        loginButton.setOnAction(x -> performAuthentication());
+    }
+
+    private void performAuthentication() {
+        Stage waitingPopup = popupFactory.createWaitingPopup("Connecting to server...");
+        waitingPopup.show();
+        String login = loginTextField.getText();
+        String password = passwordTextField.getText();
+
+        System.out.println("Your login: " + login + ", password: " + password);
+    }
+
+    private void exitButtonInitialize() {
+        exitButton.setOnAction(x -> getStage().close());
     }
 }
